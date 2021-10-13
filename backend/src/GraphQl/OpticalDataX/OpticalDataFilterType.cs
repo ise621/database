df9bfba28fa5646e4ffdd1052dbfd93c94de7039
Simon Wacker <simon.wacker@ise.fraunhofer.de>
2021-10-13 17:25:07 +0200

    Filter optical data by resources, approvals, and warnings

--- backend/src/GraphQl/OpticalDataX/OpticalDataFilterType.cs
+++ backend/src/GraphQl/OpticalDataX/OpticalDataFilterType.cs
@@ -11,11 +11,16 @@ namespace Database.GraphQl.OpticalDataX
         {
             descriptor.BindFieldsExplicitly();
             descriptor.Field(x => x.Id);
+            descriptor.Field(x => x.Locale);
             descriptor.Field(x => x.Name);
             descriptor.Field(x => x.Description);
             descriptor.Field(x => x.ComponentId);
             descriptor.Field(x => x.CreatorId);
             descriptor.Field(x => x.CreatedAt);
+            // TODO descriptor.Field(x => x.AppliedMethod);
+            descriptor.Field(x => x.Approvals);
+            descriptor.Field(x => x.Resources);
+            descriptor.Field(x => x.Warnings);
             descriptor.Field(x => x.NearnormalHemisphericalSolarReflectances);
             descriptor.Field(x => x.NearnormalHemisphericalSolarTransmittances);
             descriptor.Field(x => x.NearnormalHemisphericalVisibleReflectances);
