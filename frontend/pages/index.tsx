4cbdc4128a4f4d04b86b9096ffe5812630d5f338
Simon Wacker <simon.wacker@ise.fraunhofer.de>
2023-02-16 10:17:06 +0100

    Name `Index` page component but `Page`

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -3,14 +3,16 @@ import { Typography } from "antd";
 import Link from "next/link";
 import paths from "../paths";
 
-const Index = () => {
-  return <Layout>
+function Page() {
+  return (
+    <Layout>
       <div style={{ maxWidth: 768 }}>
         <Typography.Paragraph>
           <Link href={paths.home}>solarbuildingenvelopes.com</Link> is an example of a database in the network <Typography.Link href="https://www.buildingenvelopedata.org">Building Envelope Data</Typography.Link>. It provides access to some data of the <Typography.Link href="https://www.ise.fraunhofer.de/en/rd-infrastructure/accredited-labs/testlab-solar-facades.html">TestLab Solar Façades</Typography.Link> which is an accredited laboratory at <Typography.Link href="https://www.ise.fraunhofer.de">Fraunhofer Institute for Solar Energy Systems (ISE)</Typography.Link>. The database is a live instance of the <Typography.Link href="https://github.com/building-envelope-data/database">reference implementation</Typography.Link>.
       </Typography.Paragraph>
       </div>
-  </Layout>;
-};
+    </Layout>
+  );
+}
 
-export default Index;
+export default Page;
