48da888bb281d978c252afa471dd6f687428d170
Simon Wacker <simon.wacker@ise.fraunhofer.de>
2023-04-19 15:03:25 +0200

    Describe the TestLabDB solarbuildingenvelopes.com on its home page --- closes #42

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -2,31 +2,155 @@ import Layout from "../components/Layout";
 import { Typography } from "antd";
 import Link from "next/link";
 import paths from "../paths";
+import Image from "next/image";
+import overviewImage from "../public/overview.png";
 
 function Page() {
   return (
     <Layout>
       <div style={{ maxWidth: 768 }}>
         <Typography.Paragraph>
-          <Link href={paths.home}>solarbuildingenvelopes.com</Link> is an
-          example of a database in the network{" "}
-          <Typography.Link href="https://www.buildingenvelopedata.org">
-            Building Envelope Data
-          </Typography.Link>
-          . It provides access to some data of the{" "}
+          <Link href={paths.home}>solarbuildingenvelopes.com</Link> is the
+          website of the product data server of{" "}
           <Typography.Link href="https://www.ise.fraunhofer.de/en/rd-infrastructure/accredited-labs/testlab-solar-facades.html">
-            TestLab Solar Façades
+            TestLab Solar Facades
           </Typography.Link>{" "}
-          which is an accredited laboratory at{" "}
+          at{" "}
           <Typography.Link href="https://www.ise.fraunhofer.de">
-            Fraunhofer Institute for Solar Energy Systems (ISE)
+            Fraunhofer Institute for Solar Energy Systems ISE
           </Typography.Link>
-          . The database is a live instance of the{" "}
+          . The product data server is part of the Product Data Network{" "}
+          <Typography.Link href={`${process.env.NEXT_PUBLIC_METABASE_URL}`}>
+            buildingenvelopedata.org
+          </Typography.Link>
+          . The product data server is an instance of the{" "}
           <Typography.Link href="https://github.com/building-envelope-data/database">
-            reference implementation
+            reference implementation of a database
+          </Typography.Link>
+          . The{" "}
+          <Typography.Link href="https://github.com/building-envelope-data/database">
+            reference implementation of a database
+          </Typography.Link>{" "}
+          is open-source with a permissive license, so that everyone can easily
+          create their own product data server as part of the Product Data
+          Network{" "}
+          <Typography.Link href={`${process.env.NEXT_PUBLIC_METABASE_URL}`}>
+            buildingenvelopedata.org
           </Typography.Link>
           .
         </Typography.Paragraph>
+        <Typography.Paragraph>
+          This website is the frontend of the product data server of{" "}
+          <Typography.Link href="https://www.ise.fraunhofer.de/en/rd-infrastructure/accredited-labs/testlab-solar-facades.html">
+            TestLab Solar Facades
+          </Typography.Link>
+          . You can use this website to search this product data server for{" "}
+          <Link href={paths.opticalData}>optical data</Link>,{" "}
+          <Link href={paths.calorimetricData}>calorimetric data</Link> and{" "}
+          <Link href={paths.photovoltaicData}>photovoltaic data</Link>. If you
+          would like to search the entire Product Data Network{" "}
+          <Typography.Link href={`${process.env.NEXT_PUBLIC_METABASE_URL}`}>
+            buildingenvelopedata.org
+          </Typography.Link>
+          , you can search there for example for{" "}
+          <Typography.Link
+            href={`${process.env.NEXT_PUBLIC_METABASE_URL}/data/optical`}
+          >
+            optical data{" "}
+          </Typography.Link>
+          . You will find there also an overview about all building envelope{" "}
+          <Typography.Link
+            href={`${process.env.NEXT_PUBLIC_METABASE_URL}/components`}
+          >
+            {" "}
+            components
+          </Typography.Link>
+          ,{" "}
+          <Typography.Link
+            href={`${process.env.NEXT_PUBLIC_METABASE_URL}/institutions`}
+          >
+            institutions
+          </Typography.Link>
+          ,{" "}
+          <Typography.Link
+            href={`${process.env.NEXT_PUBLIC_METABASE_URL}/data-formats`}
+          >
+            data formats
+          </Typography.Link>
+          ,{" "}
+          <Typography.Link
+            href={`${process.env.NEXT_PUBLIC_METABASE_URL}/methods`}
+          >
+            methods
+          </Typography.Link>{" "}
+          and{" "}
+          <Typography.Link
+            href={`${process.env.NEXT_PUBLIC_METABASE_URL}/databases`}
+          >
+            databases
+          </Typography.Link>{" "}
+          of the Product Data Network{" "}
+          <Typography.Link href={`${process.env.NEXT_PUBLIC_METABASE_URL}`}>
+            buildingenvelopedata.org
+          </Typography.Link>
+          .
+        </Typography.Paragraph>
+        <Typography.Paragraph>
+          This website is completely based on the
+          <Typography.Link
+            href={`${process.env.NEXT_PUBLIC_DATABASE_URL}/graphql/`}
+          >
+            {" "}
+            GraphQL endpoint
+          </Typography.Link>
+          . The{" "}
+          <Typography.Link
+            href={`${process.env.NEXT_PUBLIC_DATABASE_URL}/graphql/`}
+          >
+            GraphQL endpoint
+          </Typography.Link>{" "}
+          is the Application Programming Interface (API) to the backend of the
+          product data server. If you like programming, you can use the endpoint
+          to automate the interaction with this product data server. You find
+          the{" "}
+          <Typography.Link href="https://github.com/building-envelope-data/api">
+            specification of the API on GitHub
+          </Typography.Link>{" "}
+          which is especially helpful if you develop software to plan buildings.
+          The{" "}
+          <Typography.Link
+            href={`${process.env.NEXT_PUBLIC_METABASE_URL}/graphql/`}
+          >
+            GraphQL endpoint of the metabase
+          </Typography.Link>{" "}
+          is then a convenient way to query for data, components, institutions,
+          databases, data formats and methods.
+        </Typography.Paragraph>
+        <Typography.Paragraph>
+          The data format{" "}
+          <Typography.Link
+            href={`${process.env.NEXT_PUBLIC_METABASE_URL}/data-formats/9ca9e8f5-94bf-4fdd-81e3-31a58d7ca708`}
+          >
+            BED-JSON
+          </Typography.Link>{" "}
+          is a general format for optical, calorimetric and photovoltaic data
+          sets. It is defined by the{" "}
+          <Typography.Link href="https://github.com/building-envelope-data/api/tree/develop/schemas">
+            JSON Schemas of the API specification
+          </Typography.Link>
+          . Other{" "}
+          <Typography.Link
+            href={`${process.env.NEXT_PUBLIC_METABASE_URL}/data-formats`}
+          >
+            data formats
+          </Typography.Link>{" "}
+          are available, too.
+        </Typography.Paragraph>
+        <Image
+          src={overviewImage}
+          alt="Schematic depiction of how users like architects, planners, or engineers can use the metabase to find products and data in and across databases."
+          style={{ maxWidth: "100%", height: "auto" }}
+        />
       </div>
     </Layout>
   );
