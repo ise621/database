a0f72df6ad3f55452380e001f79e789127d68808
Simon Wacker <simon.wacker@ise.fraunhofer.de>
2023-05-30 16:18:30 +0200

    Use the same logic for authenticating antiforgery token requests and GraphQL requests

--- backend/src/Configuration/GraphQlConfiguration.cs
+++ backend/src/Configuration/GraphQlConfiguration.cs
@@ -94,32 +94,13 @@ namespace Database.Configuration
             /* .UsePersistedQueryPipeline(); */
             .AddHttpRequestInterceptor(async (httpContext, requestExecutor, requestBuilder, cancellationToken) =>
             {
-                try {
-                    // HotChocolate uses the default authentication scheme, which we
-                    // set to `null` in `AuthConfiguration` to force users to be
-                    // explicit about what scheme to use when making it easier to
-                    // grasp the various authentication flows. For the Web frontend,
-                    // the database acts as OpenId Connect Client of the metabase
-                    // and uses the cookie scheme for authentication. For
-                    // third-party frontends, the database acts as resource server
-                    // and uses authorization header bearer tokens for
-                    // authentication, that is JavaScript Web Tokens (JWT), aka,
-                    // Access Tokens, provided as `Authorization` HTTP header with
-                    // the prefix `Bearer` as issued by OpenIddict. This Access
-                    // Token includes Scopes and Claims.
-                    var cookieAuthenticateResult = await httpContext.AuthenticateAsync(CookieAuthenticationDefaults.AuthenticationScheme).ConfigureAwait(false);
-                    if (cookieAuthenticateResult.Succeeded && cookieAuthenticateResult.Principal is not null)
-                    {
-                        httpContext.User = cookieAuthenticateResult.Principal;
-                    }
-                    else
-                    {
-                        var jwtAuthenticateResult = await httpContext.AuthenticateAsync(OpenIddictValidationAspNetCoreDefaults.AuthenticationScheme).ConfigureAwait(false);
-                        if (jwtAuthenticateResult.Succeeded && jwtAuthenticateResult.Principal is not null)
-                        {
-                            httpContext.User = jwtAuthenticateResult.Principal;
-                        }
-                    }
+                // HotChocolate uses the default authentication scheme, which we
+                // set to `null` in `AuthConfiguration` to force users to be
+                // explicit about what scheme to use when making it easier to
+                // grasp the various authentication flows.
+                try
+                {
+                    await HttpContextAuthentication.Authenticate(httpContext);
                 }
                 catch (Exception e)
                 {
